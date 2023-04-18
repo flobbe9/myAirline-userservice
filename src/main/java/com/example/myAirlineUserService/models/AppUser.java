b467e88f3ffcb1cbbe608ebce5e26a3147b1064e
flobbe10 <florin.schikarski@tecfox.eu>
2023-04-18 20:13:23 +0200

    feat: add some more security
    not finished yet

--- src/main/java/com/example/myAirlineUserService/models/AppUser.java
+++ src/main/java/com/example/myAirlineUserService/models/AppUser.java
@@ -1,14 +1,20 @@
 package com.example.myAirlineUserService.models;
 
+import java.util.Collection;
+
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.myAirlineUserService.models.validation.ValidAppUser;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
@@ -28,7 +34,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ValidAppUser(groups = {ValidAppUser.class})
-public class AppUser {
+public class AppUser implements UserDetails {
     
     @Id
     @GenericGenerator(name = "user_id_generator", 
@@ -36,7 +42,7 @@ public class AppUser {
                     parameters = {
                         @Parameter(name ="initial_value", value = "2")
                     })
-    @GeneratedValue(generator = "flight_id_generator")    
+    @GeneratedValue(generator = "user_id_generator")    
     private Long id;
 
     @NotBlank(message = "First name cannot be blank.", groups = {ValidAppUser.class})
@@ -45,22 +51,34 @@ public class AppUser {
     @NotBlank(message = "Surname cannot be blank.", groups = {ValidAppUser.class})
     private String surName;
 
+    // TODO: add email regex
     @NotBlank(message = "Email cannot be blank.", groups = {ValidAppUser.class})
     @EqualsAndHashCode.Include
+    @Column(unique = true)
     private String email;
 
+    // TODO: add password regex
     @NotBlank(message = "Password cannot be blank.", groups = {ValidAppUser.class})
     private String password;
 
-    @NotNull(message = "User role cannot be null.", groups = {ValidAppUser.class})
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "User role cannot be null", groups = {ValidAppUser.class})
     private AppUserRole role;
+
+    private boolean isAccountNonExpired = true;
+
+    private boolean isAccountNonLocked = true;
+
+    private boolean isCredentialsNonExpired = true;
+
+    private boolean isEnabled = false;
 
 
     public AppUser(@NotBlank(message = "First name cannot be blank.", groups = ValidAppUser.class) String firstName,
                     @NotBlank(message = "Surname cannot be blank.", groups = ValidAppUser.class) String surName,
                     @NotBlank(message = "Email cannot be blank.", groups = ValidAppUser.class) String email,
                     @NotBlank(message = "Password cannot be blank.", groups = ValidAppUser.class) String password,
-                    @NotNull(message = "User role cannot be null.", groups = ValidAppUser.class) AppUserRole role) {
+                    AppUserRole role) {
 
         this.firstName = firstName;
         this.surName = surName;
@@ -74,5 +92,47 @@ public class AppUser {
     public String toString() {
 
         return this.firstName;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        
+        return role.getGrantedAuthorities();
+    }
+
+
+    @Override
+    public String getUsername() {
+
+        return email;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return isAccountNonExpired;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return isAccountNonLocked;
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return isCredentialsNonExpired;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+
+        return isEnabled;
     }
 }
--- src/main/java/com/example/myAirlineUserService/models/AppUserRole.java
+++ src/main/java/com/example/myAirlineUserService/models/AppUserRole.java
@@ -1,5 +1,8 @@
 package com.example.myAirlineUserService.models;
 
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 /**
  * Enum representing the role a user can have granting them certain permissions.
@@ -8,5 +11,16 @@ package com.example.myAirlineUserService.models;
  */
 public enum AppUserRole {
     USER,
-    ADMIN
+    ADMIN;
+
+
+    /**
+     * Map all permissions of a role (including the role) as list of {@link SimpleGrantedAuthority}.
+     * 
+     * @return list of authorities coming along with a role
+     */
+    public List<SimpleGrantedAuthority> getGrantedAuthorities() {
+
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    }
 }
