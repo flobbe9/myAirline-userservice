b99be3107b4e7740ad54f62c4d081b92b1c0f115
flobbe10 <florin.schikarski@tecfox.eu>
2023-04-18 14:28:25 +0200

    feat: add mock user and postgres

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO app_user(id, first_name, sur_name, email, password, role, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled) VALUES(1, 'Max', 'Mustermann', 'musteremail@domain.com', 'password', 'USER', true, true, true, false);
