31e40c5931357934b77dbf829472771e78c1326b
flobbe10 <florin.schikarski@tecfox.eu>
2023-04-21 14:39:53 +0200

    fix: delete folders for gitignore

--- build/resources/main/data.sql
+++
@@ -1,2 +0,0 @@
--- Password: password
-INSERT INTO app_user(id, first_name, sur_name, email, password, role, is_account_non_expired, is_account_non_locked, is_credentials_non_expired, is_enabled) VALUES(1, 'Max', 'Mustermann', 'musteremail@domain.com', '$2a$10$d7aGVPSVCCmbEyuSyOid1.jI4hoUEPBgNUL1Fy1QFxDPdQ0kxG.mq', 'USER', true, true, true, true);
