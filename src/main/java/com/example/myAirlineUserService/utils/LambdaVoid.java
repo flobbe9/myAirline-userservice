22012349e29bccedaa3b0f90c59eda8d9ed41777
flobbe10 <florin.schikarski@tecfox.eu>
2023-04-23 18:48:11 +0200

    feat: add two interfaces to use lambdas

---
+++ src/main/java/com/example/myAirlineUserService/utils/LambdaVoid.java
@@ -0,0 +1,13 @@
+package com.example.myAirlineUserService.utils;
+
+
+/**
+ * Interface defining a void function in order to use it as lambda in a
+ * function parameter.
+ * 
+ * @since 0.0.1
+ */
+public interface LambdaVoid {
+    
+    void returnsNothing();
+}
